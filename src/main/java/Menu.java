59ae067b80e8f5d362abb9563f4a21658427f15f
Alexandrine <nicolas.auden@gmail.com>
2019-01-21 20:44:26 +0100

    refonte du menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,106 +1,63 @@
 package main.java;
 
 import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Menu {
+
+    Properties prop = new Properties();
+    InputStream input = null;
 
     /**
      * Display all games choices
      */
-    public void displayAvailableGame() {
-        System.out.println("Choix jeu");
-        System.out.println("1 - Mastermind");
-        System.out.println("2 - Recherche +/-");
-        System.out.println("À quel jeu souhaitez-vous jouer ?");
-    }
+    public Menu() {
 
-    /**
-     * Display a game selected
-     * @param nbGame The selected game
-     */
-    public void displaySelectedGame (int nbGame) {
-        switch (nbGame) {
-            case 1:
-                System.out.println("Vous avez choisi comme jeu : Mastermind");
-                break;
-            case 2:
-                System.out.println("Vous avez choisi comme jeu : Recheche +/-");
-                break;
-                default:
-                    System.out.println("Vous n'avez choisi aucun jeu parmi les choix proposés");
-                    break;
-        }
-    }
+        byte choix = 0;
 
-    /**
-     * Run asking process for a game
-     */
-    public void runGame () {
-        this.displayAvailableGame();
-        Scanner sc = new Scanner(System.in);
-        int nbGame = sc.nextInt();
-        this.displaySelectedGame(nbGame);
-        switch (nbGame) {
-            case 1:
-                displayAvailableSide(true);
-                int nbSide = sc.nextInt();
-                displaySelectedSide(nbSide, true);
-                break;
-            case 2:
-                displayAvailableSide(true);
-                nbSide = sc.nextInt();
-                displaySelectedSide(nbSide, true);
-                break;
-            case 3:
-                displayAvailableSide(false);
-                nbSide = sc.nextInt();
-                displaySelectedSide(nbSide, false);
-                break;
-        }
-    }
+        System.out.println("-------------  Bienvenue dans l'application MasterMind !  -------------\n\n");
+        System.out.println("Merci de sélectionner le jeu de votre choix. Have fun !\n");
 
-    /**
-     * Display all avalaible sides depending on all sides enable or not
-     * All sides = challenger, défenseur and duel
-     * No all sides = challenger or duel for example
-     * @param allSideEnable enable display for all side or not
-     */
-    public void displayAvailableSide (boolean allSideEnable) {
-        System.out.println("Choix du mode de jeu");
-        if (allSideEnable) {
-            System.out.println("1 - challenger");
-            System.out.println("2 - défenseur");
-            System.out.println("3 - duel");
-        } else {
-            System.out.println("1 - Vous n'avez choisi aucun jeu parmi les choix proposés");
-        }
-        System.out.println("Quel mode de jeu souhaitez-vous sélectionner ?");
-    }
+        do {
 
-    /**
-     * Display a selected side depending on all sides enable or not
-     * All sides = challenger, défenseur and duel
-     * No all sides = challenger or duel for example
-     * @param nbSide the selected Side
-     * @param allSidesEnable enable display for all side or not
-     */
-    public void displaySelectedSide (int nbSide, boolean allSidesEnable) {
-        if (allSidesEnable) {
-            switch (nbSide) {
-                case 1:
-                    System.out.println("Vous avez choisi comme mode de jeu : challenger");
-                    break;
+            System.out.println("Choix jeu :");
+            System.out.println("1 - Mastermind");
+            System.out.println("2 - Recherche +/-");
+            System.out.println("3 - Exit");
+            System.out.println("À quel jeu souhaitez-vous jouer ?");
+
+            Scanner sc = new Scanner(System.in);
+
+            // Si l'entrée clavier n'est pas un byte
+            if (!sc.hasNextByte()) {
+                Erreur.erreurChoix();
+                sc.next();
+                continue;
+            }
+            choix = sc.nextByte();
+            switch (choix) {
+
+                /*case 1:
+                    Mastermind mastermind = new Mastermind();// Mastermind
+                    choix = 0;
+                    System.out.println("Vous avez choisi comme jeu : Mastermind");
+                    break;*/
                 case 2:
-                    System.out.println("Vous avez choisi comme mode de jeu : défenseur");
+                    PlusOuMoins plusOuMoins = new PlusOuMoins();
+                    choix = 0;
+                    System.out.println("Vous avez choisi comme jeu : Recherche +/-");
                     break;
                 case 3:
-                    System.out.println("Vous avez choisi comme mode de jeu : duel");
+                    System.out.println("À bientôt !");
                     break;
-                    default:
-                        System.out.println("Vous n'avez choisi aucun mode de jeu parmi les choix proposés");
-                        break;
+                default:
+                    Erreur.erreurChoix();
+                    System.out.println("Vous n'avez choisi aucun jeu parmi les choix proposés");
+                    break;
             }
+            }
+            while (choix != 1 && choix != 2 && choix != 3 && choix != 4) ;
         }
     }
 
-}
